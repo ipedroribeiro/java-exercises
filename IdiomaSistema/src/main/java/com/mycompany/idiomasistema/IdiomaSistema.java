90fb3320dfd02221b56c956c050eaf42f90cd51c
Pedro Ribeiro <97313521+ipedroribeiro@users.noreply.github.com>
2023-06-27 13:25:59 -0300

    Add files via upload

---
+++ IdiomaSistema/src/main/java/com/mycompany/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.idiomasistema;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author pdped
+ */
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        Locale Idioma= Locale.getDefault();
+            System.out.print("A máquina está no idioma");
+            System.out.print(Idioma);
+    }
+}
