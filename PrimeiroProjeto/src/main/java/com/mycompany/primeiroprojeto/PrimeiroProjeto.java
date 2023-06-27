871cdf582ff438d4e5addb7f79ce69a345b817e8
Pedro Ribeiro <97313521+ipedroribeiro@users.noreply.github.com>
2023-06-27 13:32:48 -0300

    Add files via upload

---
+++ PrimeiroProjeto/src/main/java/com/mycompany/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprojeto;
+
+
+public class PrimeiroProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
---
+++ ProgramaMedia/src/programamedia/ProgramaMedia.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programamedia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pdped
+ */
+public class ProgramaMedia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner (System.in);
+        System.out.print("Digite sua primeira nota: ");
+        float n1 = teclado.nextFloat();
+        System.out.print("Digite sua segunda nota: ");
+        float n2 = teclado.nextFloat();
+        float m = (n1 + n2) / 2;
+        System.out.println("Sua média foi " + m);
+        if (m > 9) {
+            System.out.println("PARABÉNS!! Você é incrível.");
+        }
+    
+    
+    }
+    
+}
